e589cea7a197846817bfe95f494c4f547a89adf5
Mateus Veloso <mfvelosos@gmail.com>
2023-09-03 22:44:02 -0300

    add xid lib and increase test coverage of campaigns

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Body"
@@ -14,8 +15,30 @@ func TestNewCampaign(t *testing.T) {
 
 	campaign := NewCampaign(name, content, emails)
 
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(emails))
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Body"
+	emails := []string{"email1@test.com", "email2@test.com"}
+
+	campaign := NewCampaign(name, content, emails)
+
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOnIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Body"
+	emails := []string{"email1@test.com", "email2@test.com"}
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, emails)
+
+	assert.Greater(campaign.CreatedOn, now)
 }
