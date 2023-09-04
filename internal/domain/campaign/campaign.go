8222443603a969b29290c20721a8be91c7d6f769
Mateus Veloso <mfvelosos@gmail.com>
2023-09-03 23:11:48 -0300

    add validations and increase test Bcoverage

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"time"
 
 	"github.com/rs/xid"
@@ -18,7 +19,15 @@ type Campaign struct {
 	Contacts  []Contact
 }
 
-func NewCampaign(name string, content string, emails []string) *Campaign {
+func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
+
+	if name == "" {
+		return nil, errors.New("name is required")
+	} else if content == "" {
+		return nil, errors.New("content is required")
+	} else if len(emails) == 0 {
+		return nil, errors.New("emails are required")
+	}
 
 	contacs := make([]Contact, len(emails))
 	for idx, email := range emails {
@@ -31,5 +40,5 @@ func NewCampaign(name string, content string, emails []string) *Campaign {
 		CreatedOn: time.Now(),
 		Content:   content,
 		Contacts:  contacs,
-	}
+	}, nil
 }
--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -16,7 +16,7 @@ var (
 func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
 
-	campaign := NewCampaign(name, content, emails)
+	campaign, _ := NewCampaign(name, content, emails)
 
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
@@ -26,7 +26,7 @@ func Test_NewCampaign_CreateCampaign(t *testing.T) {
 func Test_NewCampaign_IDIsNotNil(t *testing.T) {
 	assert := assert.New(t)
 
-	campaign := NewCampaign(name, content, emails)
+	campaign, _ := NewCampaign(name, content, emails)
 
 	assert.NotNil(campaign.ID)
 }
@@ -35,7 +35,31 @@ func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
 	assert := assert.New(t)
 	now := time.Now().Add(-time.Minute)
 
-	campaign := NewCampaign(name, content, emails)
+	campaign, _ := NewCampaign(name, content, emails)
 
 	assert.Greater(campaign.CreatedOn, now)
+}
+
+func Test_NewCampaign_ValidatesName(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign("", content, emails)
+
+	assert.Equal("name is required", err.Error())
+}
+
+func Test_NewCampaign_ValidatesContent(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, "", emails)
+
+	assert.Equal("content is required", err.Error())
+}
+
+func Test_NewCampaign_ValidatesEmails(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := NewCampaign(name, content, []string{})
+
+	assert.Equal("emails are required", err.Error())
 }
